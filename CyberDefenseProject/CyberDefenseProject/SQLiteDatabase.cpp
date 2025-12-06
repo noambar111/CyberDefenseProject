6fd0e3ddedab38b05c2bad57245e6394ff2a9822
unknown <noam051@gmail.com>
2025-12-06 12:01:34 +0200

    feat (WebServer): add concrete sql class - implements: CTOR, DTOR, saveGeo, getCountriesByIp.

--- CyberDefenseProject/CyberDefenseProject/SQLiteDatabase.cpp
+++ CyberDefenseProject/CyberDefenseProject/SQLiteDatabase.cpp
@@ -19,6 +19,52 @@ SQLiteDatabase::~SQLiteDatabase()
 	if (m_db) sqlite3_close(m_db);
 }
 
+bool SQLiteDatabase::saveGeo(const GeoRecord& rec)
+{
+	const char* sql = "INSTERT INTO geo_records (ip, country) VALUES (?, ?);";
+
+	sqlite3_stmt* stmt = nullptr;
+	sqlite3_prepare_v2(m_db, sql, -1, &stmt, nullptr);
+
+	sqlite3_bind_text(stmt, 1, rec.ip.c_str(), -1, SQLITE_TRANSIENT);
+	sqlite3_bind_text(stmt, 2, rec.country.c_str(), -1, SQLITE_TRANSIENT);
+
+	bool ok = (sqlite3_step(stmt) == SQLITE_DONE);
+	sqlite3_finalize(stmt);
+
+	return ok;
+}
+
+std::optional<std::string> SQLiteDatabase::getCountryByIP(const std::string& ip)
+{
+	const char* sql = "SELECT country FROM geo_records WHERE ip = ? ORDER BY id DESC LIMIT 1;";
+
+	sqlite3_stmt* stmt;
+	sqlite3_prepare_v2(m_db, sql, -1, &stmt, nullptr);
+	sqlite3_bind_text(stmt, 1, ip.c_str(), -1, SQLITE_TRANSIENT);
+
+	if (sqlite3_step(stmt) == SQLITE_ROW)
+	{
+		std::string result = (const char*)sqlite3_column_text(stmt, 0);
+		sqlite3_finalize(stmt);
+		return result;
+	}
+
+
+	sqlite3_finalize(stmt);
+	return std::nullopt;
+}
+
+std::vector<std::string> SQLiteDatabase::getIPsByCountry(const std::string& country)
+{
+	return std::vector<std::string>();
+}
+
+std::vector<std::pair<std::string, int>> SQLiteDatabase::getTopCountries(int limit)
+{
+	return std::vector<std::pair<std::string, int>>();
+}
+
 void SQLiteDatabase::initSchema()
 {
 	const char* sql =
